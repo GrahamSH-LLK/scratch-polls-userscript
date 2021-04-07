25b25c8e065dd889ce37e00ee689643f3bd507ab
GrahamSH <grahamshllk@gmail.com>
2021-04-07 16:42:35 -0400

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    https://scratch.mit.edu
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://scratch.mit.edu/discuss/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM_addStyle
+// @require      https://gh.grahamsh.com/scratch-polls/main.js
+// ==/UserScript==
+
+console.log('Scratch polls loaded')
